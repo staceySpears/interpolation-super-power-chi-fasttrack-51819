01e169d1fec0c2c7d8ca9bc615155f367610a088
creative-loop-3505 <staceyspears2@gmail.com>
2019-05-19 09:51:30 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,4 +2,5 @@
 def 
   print display_rainbow('red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet')
   puts "R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet"
+end
   
